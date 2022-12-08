61ab775952d8d6f73133d2828b5309c34acd1889
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-12-08 19:02:20 -0300

    searching vercel error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,8 +46,8 @@ export default function Home({ places }: HomeProps) {
           >
             <PlaceCard {...item} />
           </div>
-        ))}
-      </main> */}
+        ))} */}
+      </main>
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
