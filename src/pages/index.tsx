2df9e9a1094f38f0fa09c569b5e47fdc3e4bb8c0
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-12-08 20:02:53 -0300

    vercel fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,9 +50,8 @@ export default function Home({ places }: HomeProps) {
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://github.com/lucarampi/valtech-challenge"
+          target="_blank" rel="noreferrer"
         >
           Developed by <span className={styles.logo}>Luca A.R.</span>
         </a>
