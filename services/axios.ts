48e2bcbef62539f7eb86a6d7c18af0d1e36be1ce
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-12-08 22:22:15 -0300

    axios error

--- services/axios.ts
+++ services/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL
-    }` : "http://localhost:3000"
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000"
 
 const api = axios.create({
     baseURL: baseURL
