bae4a7669c112951bf7e8c9ecc74a494c7b5c8e7
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-12-08 21:40:14 -0300

    google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import "../styles/globals.css";
 export default function RootLayout({
@@ -8,6 +9,12 @@ export default function RootLayout({
   return (
     <html>
       <head />
+      <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
       <body>{children}</body>
     </html>
   );
